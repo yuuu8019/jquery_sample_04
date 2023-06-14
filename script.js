48bbb0f17263f1840777072a119b25474da4f2f9
yuuu8019 <131326903+yuuu8019@users.noreply.github.com>
2023-06-14 20:26:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Smooth Scrool Sample
+
+$(function(){
+
+   $('a[href^="#"]').click(function() {
+
+      // 初期設定：移動時間(ms)と頭出し位置
+      var speed = 500;
+      var offset = -0;
+
+      // アンカーを取得
+      var anchor = $(this).attr("href");
+
+      // ターゲットの位置を取得
+      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+      var position = target.offset().top + offset;
+
+      // スクロール（アニメーション）
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+      return false;
+
+   });
+
+});
